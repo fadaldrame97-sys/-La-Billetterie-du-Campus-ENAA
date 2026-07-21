e231c395c7268f98ca82650a30ae264748e12354
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-07-21 12:23:47 +0100

    Créer la correspondance aux champs de la table events

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -3,8 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Event extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'description',
+        'date',
+        'time',
+        'location',
+        'price',
+        'capacity',
+    ];
+
+    public function reservations():HasMany
+    {
+        return $this->hasMany(Reservation::class);
+    }
 }
