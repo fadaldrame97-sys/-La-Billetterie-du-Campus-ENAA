0b8ffe284b2667c15fb61c8155a58754fbff4df7
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-07-21 12:40:12 +0100

    Créer la relation belongsTo entre Event et Reservation

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Reservation extends Model
 {
@@ -12,9 +13,13 @@ class Reservation extends Model
         'ticket_code',
     ];
 
-    public function User(){
+    public function User():BelongsTo{
         return $this->belongsTo(User::class);
     }
 
-    
+    public function Event():BelongsTo {
+        return $this->belongsTo(Event::class);
+    }
+
+
 }
