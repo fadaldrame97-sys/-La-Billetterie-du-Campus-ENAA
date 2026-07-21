727915a4ff8fb337c5397cacd3488d532c1b6ecf
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-07-21 15:26:05 +0100

    Créer les route de l'admin protégées

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\ReservationController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::middleware(['auth', 'isAdmin'])->group(function () {
+
+    Route::get('/admin/dashboard', [EventController::class, 'index'])
+        ->name('admin.dashboard');
+
+    Route::get('/admin/events/create', [EventController::class, 'create'])
+        ->name('events.create');
+
+    Route::post('/admin/events', [EventController::class, 'store'])
+        ->name('events.store');
+
 });
