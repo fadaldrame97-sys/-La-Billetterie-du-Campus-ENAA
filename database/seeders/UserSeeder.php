0f0d31c0a5dc5feb8bf60631dcaa411a32554b38
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-07-21 18:06:42 +0100

    Créer des utilisateur student

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,10 +14,43 @@ class UserSeeder extends Seeder
     {
         User::create([
             'name'=>'Admin',
-            'email'=>'Admin@gmail.com',
+            'email'=>'admin@gmail.com',
             'password'=>Hash::make('password'),
             'role'=>'admin'
 
         ]);
+
+
+
+         $etudiants = [
+
+        ['name' => 'Fadal Dramé', 'email' => 'fadal@gmail.com'],
+         ['name' => 'Mouhamad Ba', 'email' => 'ba@gmail.com'],
+        ['name' => 'Babacar Faye', 'email' => 'babacar@gmail.com'],
+        ['name' => 'Boubacar Ba', 'email' => 'boubacar@gmail.com'],
+        ['name' => 'Fatou Ba', 'email' => 'fatou@gmail.com'],
+        ['name' => 'Khalifa Sylla', 'email' => 'sylla@gmail.com'],
+        ['name' => 'Fatima Diallo', 'email' => 'diallo@gmail.com'],
+        ['name' => 'Coumba Sy', 'email' => 'sy@gmail.com'],
+        ['name' => 'Moustapha Samb', 'email' => 'moustapha@gmail.com'],
+        ['name' => 'Aminata Fall', 'email' => 'fall@gmail.com'],
+        ['name' => 'Ibrahima Diop', 'email' => 'diop@gmail.com'],
+        ['name' => 'Mariama Diakhaté', 'email' => 'diakhate@gmail.com'],
+        ['name' => 'Sokhna Bousso Faye', 'email' => 'faye@gmail.com'],
+        ['name' => 'Abdou Ahad Bousso', 'email' => 'bousso@gmail.com'],
+        ['name' => 'Abdoulahi niang', 'email' => 'niang@gmail.com'],
+        ['name' => 'Oumoul Khairy', 'email' => 'oummou@gmail.com'],
+
+    ];
+
+    foreach ($etudiants as $etudiant) {
+
+        User::create([
+            'name' => $etudiant['name'],
+            'email' => $etudiant['email'],
+            'password' => Hash::make('password'),
+            'role' => 'student',
+        ]);
+    }
     }
 }
