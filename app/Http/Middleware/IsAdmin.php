06fd72a7aaa4c29531df70aafcf711e40f28c397
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-07-21 15:50:38 +0100

    Function pour la proctection de isAdmin

---
+++ app/Http/Middleware/IsAdmin.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use App\Http\Models;
+use Illuminate\Support\Facades\Auth;
+
+class IsAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Closure(Request): (Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+
+        if(auth()->check() && auth()->user()->role =='admin'){
+             
+            return $next($request);
+
+        }
+        
+            abort(403);
+        
+       
+    }
+}
