7494b654a59b4d56b8f081e3a926ba9b73b534f6
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-07-21 16:06:13 +0100

    Correction de syntaxe

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
 
-      $middleware->alias(['isAdmin'=>App\Http\Middleware\IsAdmin::class]);
+      $middleware->alias(['isAdmin'=>\App\Http\Middleware\IsAdmin::class]);
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
