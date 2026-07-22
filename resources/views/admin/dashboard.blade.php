8eca1bb4241305f6cc075fbfd039ebe8f3313bc1
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-07-22 16:25:42 +0100

    ajouter le lien de navigation de events/create dans blade dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -5,6 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+
+    <a href="{{ route('events.create') }}">
+    Créer un événement
+</a>
 </head>
 <body>
     <h1>Dashboard Admin</h1>
