e7aaab14b1152f4253dd7c8d53a73ed7f16f3fd6
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-07-22 11:25:09 +0100

    Créer une function pour afficher le login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,5 +6,18 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function showLogin()
+    {
+       return view('auth.login');
+    }
+
+    public function login(Request $request)
+    {
+
+    }
+
+    public function logout(Request $request)
+    {
+
+    }
 }
