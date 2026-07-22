82caf7c065d2c837609d395b4fa49fc15bd0b381
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-07-22 23:39:28 +0100

    Ajouter logique pour éviter double reservation pour sur un event

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -11,16 +11,27 @@ class ReservationController extends Controller
 {
     public function store(Event $event){
 
+
+
+        $ReserveDeja=Reservation::where('user_id',Auth::id())->where('event_id',$event->id)->first();
+        if($ReserveDeja)  return back()->with('error', 'Vous avez déja réservé pour cet évènement !');
+
         $nombreReservations=Reservation::where('event_id',$event->id)->count();
         if($nombreReservations>= $event->capacity){
             return back()->with('error', 'Toutes les place sont occupées');
         }
+
+
+        
 
         Reservation::create([
         'user_id' => Auth::id(),
         'event_id' => $event->id,
         'ticket_code' => 'PASS'.uniqid(),
        ]);
+
+      
+
 
        return redirect()->route('events.index');
 
