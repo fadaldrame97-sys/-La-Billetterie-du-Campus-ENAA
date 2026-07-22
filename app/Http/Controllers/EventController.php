2d7d807247edc27b20b979d1998e82f27ae867d9
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-07-22 15:29:08 +0100

    Créer les redirections dans EventController

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -6,5 +6,18 @@ use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('admin.dashboard');
+    }
+
+    public function create()
+    {
+        return view('admin.events.create');
+    }
+
+    public function store(Request $request)
+    {
+
+    }
 }
