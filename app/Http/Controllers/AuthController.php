d4d9cd0087a4b609562ac8e2ce99ca83f2702cbc
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-07-22 11:52:17 +0100

    si le user est admin redrigé vers dashboard admin

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -18,7 +19,20 @@ class AuthController extends Controller
          'password' => 'required',
         ]);
 
-    }
+            if (Auth::attempt([
+              'email' => $request->email,
+              'password' => $request->password,
+        ])) {
+
+          $request->session()->regenerate();
+
+        }
+
+        if (Auth::user()->role == 'admin') {
+
+            return redirect()->route('admin.dashboard');
+
+        }
 
     public function logout(Request $request)
     {
