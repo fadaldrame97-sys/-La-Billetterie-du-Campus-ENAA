54fe6061896d6f55b3db0c72395b1e0f40513929
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-07-22 15:40:48 +0100

    Ajouter une nouvelle route pour orienté le student conncté

--- routes/web.php
+++ routes/web.php
@@ -19,13 +19,11 @@ Route::post('/logout',[AuthController::class,'logout'])->middleware('auth')->nam
 
 Route::middleware(['auth', 'isAdmin'])->group(function () {
 
-    Route::get('/admin/dashboard', [EventController::class, 'index'])
-        ->name('admin.dashboard');
+    Route::get('/admin/dashboard', [EventController::class, 'index'])->name('admin.dashboard');
 
-    Route::get('/admin/events/create', [EventController::class, 'create'])
-        ->name('events.create');
+    Route::get('/admin/events/create', [EventController::class, 'create'])->name('events.create');
 
-    Route::post('/admin/events', [EventController::class, 'store'])
-        ->name('events.store');
-
+    Route::post('/admin/events', [EventController::class, 'store'])->name('events.store');
+    
+    Route::get('/events', [EventController::class, 'liste'])->name('events.index');
 });
