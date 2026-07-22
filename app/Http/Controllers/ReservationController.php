b1a1efc638be81efc9e55245161ba0cc07b52c63
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-07-22 23:13:12 +0100

    Créer la reservation

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Reservation;
 use App\Models\Event;
+use Illuminate\Support\Facades\Auth;
+
 class ReservationController extends Controller
 {
     public function store(Event $event){
@@ -13,6 +15,12 @@ class ReservationController extends Controller
         if($reservation>= $event->capacity){
             return back()->with('error', 'Toutes les place sont occupées');
         }
+
+        Reservation::create([
+        'user_id' => Auth::id(),
+        'event_id' => $event->id,
+        'ticket_code' => '',
+]);
 
     }
 }
