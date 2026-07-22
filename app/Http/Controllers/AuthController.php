839b4356073bb542c88e46a90150bc37dd5c2f30
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-07-22 11:47:39 +0100

    Validation de login régénérer l'identifiant de session

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -13,6 +13,10 @@ class AuthController extends Controller
 
     public function login(Request $request)
     {
+        $request->validate([
+         'email' => 'required|email',
+         'password' => 'required',
+        ]);
 
     }
 
