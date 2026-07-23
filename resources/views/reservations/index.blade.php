11a597de71be680e9c4e6e38767fe974d1cd32de
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-07-23 11:24:38 +0100

    Afficer un message d'errur ou succes après la réservation

--- resources/views/reservations/index.blade.php
+++ resources/views/reservations/index.blade.php
@@ -9,6 +9,12 @@
 </head>
 <body>
 
+    @if(session('success'))
+    <div class="bg-green-100 text-green-700 p-3 rounded mb-4">
+        {{ session('success') }}
+    </div>
+    @endif
+
     <h1>Mes Billets</h1>
 
 @foreach($reservations as $reservation)
