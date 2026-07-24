79f567f1ed65fbb1d2ee263578e501297cca861c
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-07-24 12:22:30 +0100

    Créer la route pour modifier un event

--- routes/web.php
+++ routes/web.php
@@ -24,13 +24,17 @@ Route::get('/mes-Pass',[ReservationController::class,'index'])->middleware('auth
 
 Route::middleware(['auth', 'isAdmin'])->group(function () {
 
-    Route::get('/admin/event/{event}/edit',[EventController::class,'edit'])->name('events.edit');
+   
 
     Route::get('/admin/dashboard', [EventController::class, 'index'])->name('admin.dashboard');
 
     Route::get('/admin/events/create', [EventController::class, 'create'])->name('events.create');
 
     Route::post('/admin/events', [EventController::class, 'store'])->name('events.store');
+
+    Route::get('/admin/event/{event}/edit',[EventController::class,'edit'])->name('events.edit');
+
+    Route::put('/admin/events/{event}', [EventController::class, 'update'])->name('events.update');
     
     
 });
