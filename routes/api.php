5318bcaffad9ef087d8920b5f067eb6d40f8eb0d
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-08-05 13:29:11 +0100

    Créer des routes publique des évenements

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\EventApiController;
 use App\Http\Controllers\Api\ReservationApiController;
 use App\Http\Controllers\Api\AuthApiController;
+
+
+Route::get('/events', [EventApiController::class, 'index']);
+Route::get('/events/{event}', [EventApiController::class, 'show']);
+
