7784badb405e0142db2f6165fb845b69b997c706
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-08-08 00:55:14 +0100

    Connecter le user

--- app/Http/Controllers/api/AuthApiController.php
+++ app/Http/Controllers/api/AuthApiController.php
@@ -3,9 +3,40 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Services\AuthService;
 use Illuminate\Http\Request;
 
 class AuthApiController extends Controller
 {
-    //
+    public function __construct(
+        protected AuthService $authService
+    ) {}
+
+    public function login(LoginRequest $request)
+    {
+        $token = $this->authService->login(
+            $request->validated()
+        );
+
+        if (!$token) {
+            return response()->json([
+                'message' => 'Email ou mot de passe incorrect.'
+            ], 401);
+        }
+
+        return response()->json([
+            'message' => 'Connexion réussie.',
+            'token' => $token
+        ]);
+    }
+
+    public function logout()
+    {
+        $this->authService->logout();
+
+        return response()->json([
+            'message' => 'Déconnexion réussie.'
+        ]);
+    }
 }
