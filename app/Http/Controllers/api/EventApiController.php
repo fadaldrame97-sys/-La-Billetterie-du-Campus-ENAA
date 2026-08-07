1d3f219fa9d0ff874133b8ead974abc2ed8d780a
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-08-07 11:18:38 +0100

    Le controller retourne le json des methodes du crud dans le services

--- app/Http/Controllers/api/EventApiController.php
+++ app/Http/Controllers/api/EventApiController.php
@@ -3,17 +3,62 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreEventRequest;
+use App\Http\Requests\UpdateEventRequest;
 use App\Models\Event;
+use App\Services\EventService;
 
 class EventApiController extends Controller
 {
-    public function index()
+    protected $eventService;
+
+    public function __construct(EventService $eventService)
     {
-        return response()->json(Event::all());
+        $this->eventService = $eventService;
     }
 
-    public function show(Event $event){
-    
-    return response()->json($event);
+    public function index()
+    {
+        return response()->json(
+            $this->eventService->getAllEvents()
+        );
+    }
+
+    public function show(Event $event)
+    {
+        return response()->json(
+            $this->eventService->getEvent($event)
+        );
+    }
+
+    public function store(StoreEventRequest $request)
+    {
+        $event = $this->eventService
+            ->createEvent($request->validated());
+
+        return response()->json([
+            'message' => 'Evénement créé avec succès.',
+            'event' => $event
+        ], 201);
+    }
+
+    public function update(UpdateEventRequest $request, Event $event)
+    {
+        $event = $this->eventService
+            ->updateEvent($event, $request->validated());
+
+        return response()->json([
+            'message' => 'Evénement modifié avec succès.',
+            'event' => $event
+        ]);
+    }
+
+    public function destroy(Event $event)
+    {
+        $this->eventService->deleteEvent($event);
+
+        return response()->json([
+            'message' => 'Evénement supprimé avec succès.'
+        ]);
     }
 }
