ac330f888bab7e9fbd487c71a232102cc2ffb6ed
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-08-08 01:09:10 +0100

    Appler le service dans le controller et retourner des réponse json

--- app/Http/Controllers/api/ReservationApiController.php
+++ app/Http/Controllers/api/ReservationApiController.php
@@ -4,9 +4,39 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Event;
-use Illuminate\Http\Request;
+use App\Services\ReservationService;
 
 class ReservationApiController extends Controller
 {
-    //
+    protected $reservationService;
+
+    public function __construct(ReservationService $reservationService)
+    {
+        $this->reservationService = $reservationService;
+    }
+
+    public function store(Event $event)
+    {
+        $reservation = $this->reservationService
+            ->createReservation($event);
+
+        if (!$reservation) {
+            return response()->json([
+                'message' => 'Vous avez déjà réservé ou l’événement est complet.'
+            ], 400);
+        }
+
+        return response()->json([
+            'message' => 'Réservation effectuée avec succès.',
+            'reservation' => $reservation
+        ], 201);
+    }
+
+    public function index()
+    {
+        $reservations = $this->reservationService
+            ->getUserReservations();
+
+        return response()->json($reservations);
+    }
 }
