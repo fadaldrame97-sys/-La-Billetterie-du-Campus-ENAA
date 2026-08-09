089940bf8fbf248dd308f758c58791e8d8e763b2
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-08-09 12:30:29 +0100

    Importer l'instance de api

--- bedEvent-React/src/App.jsx
+++ bedEvent-React/src/App.jsx
@@ -1,8 +1,5 @@
 import { useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import api from './services/api'
 
 function App() {
   const [count, setCount] = useState(0)
