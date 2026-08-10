c29f0e7cc2e371d01d9220daa2a13719ec7b8567
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-08-10 13:01:34 +0100

    Créer function App et retouner les routes des pages

--- bedEvent-React/src/App.jsx
+++ bedEvent-React/src/App.jsx
@@ -5,3 +5,36 @@ import Login from './pages/Login';
 import EventDetail from './pages/EventDetail';
 import ProfileTickets from './pages/ProfileTickets';
 import AdminDashboard from './pages/AdminDashboard';
+
+function App() {
+    return (
+        <BrowserRouter>
+
+            <Routes>
+
+                <Route path="/" element={<Home />} />
+
+                <Route path="/login" element={<Login />} />
+
+                <Route
+                    path="/events/:id"
+                    element={<EventDetail />}
+                />
+
+                <Route
+                    path="/profile/tickets"
+                    element={<ProfileTickets />}
+                />
+
+                <Route
+                    path="/admin"
+                    element={<AdminDashboard />}
+                />
+
+            </Routes>
+
+        </BrowserRouter>
+    );
+}
+
+export default App;
