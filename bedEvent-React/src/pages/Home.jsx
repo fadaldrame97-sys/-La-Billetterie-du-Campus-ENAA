40dd525ecb8f4350e9792970d355b80795e5ea8b
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-08-10 12:44:22 +0100

    Styler Home.jsx

--- bedEvent-React/src/pages/Home.jsx
+++ bedEvent-React/src/pages/Home.jsx
@@ -1,42 +1,41 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
 import api from '../services/api';
 import EventCard from '../components/EventCard';
 
 function Home() {
 
-    const [events,setEvents]=useState([]);
+    const [events, setEvents] = useState([]);
 
-    useEffect(()=>{
-      api.get('/events')
-      .then(Response=>{
-        console.log(Response.data);
-        setEvents(Response.data);
-      })
+    useEffect(() => {
+        api.get('/events')
+            .then(response => {
+                setEvents(response.data);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }, []);
 
-      .catch(error=>{
-        console.log(error)
-      });
-      
+    return (
+        <div className="max-w-6xl mx-auto p-6">
 
-  },[])
-  
+            <h1 className="text-3xl font-bold mb-6">
+                BDE-Events
+            </h1>
 
-return (
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
 
-  <div> 
-   <h1>BDE-Events</h1>
+                {events.map(event => (
+                    <EventCard
+                        key={event.id}
+                        event={event}
+                    />
+                ))}
 
-   {events.map(events=>(
+            </div>
 
-    <EventCard key={event.id} event={event}/> 
-
-   ))}
-
-   </div> 
-);
-
-
-    
+        </div>
+    );
 }
 
 export default Home;
