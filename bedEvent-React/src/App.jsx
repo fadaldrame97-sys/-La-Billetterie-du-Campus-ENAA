7b1146da46f1d6753fd45c5204560c6fdcd631fe
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-08-10 13:00:34 +0100

    importer les quatres fichier de mon components

--- bedEvent-React/src/App.jsx
+++ bedEvent-React/src/App.jsx
@@ -1,1 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+import EventDetail from './pages/EventDetail';
+import ProfileTickets from './pages/ProfileTickets';
+import AdminDashboard from './pages/AdminDashboard';
