e076fd4c8ab5b814e26799ac4e4d2cd4d751e53d
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-08-10 15:49:02 +0100

    Construire le dashboard admin et afficher les events + buttons modifier & supprimer

---
+++ bedEvent-React/src/pages/admin/AdminDashboard.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react';
+import api from '../services/api';
+
+function AdminDashboard() {
+
+    const [events, setEvents] = useState([]);
+
+    useEffect(() => {
+
+        api.get('/events')
+            .then(response => {
+                console.log(response.data);
+                setEvents(response.data);
+            })
+            .catch(error => { console.log(error);});
+
+    }, []);
+
+    return (
+        <div>
+
+            <h1>Dashboard Admin</h1>
+
+            {events.map(event => (
+                <div key={event.id}>
+                    <h2>{event.title}</h2>
+                    <p>{event.description}</p>
+                    <p> {event.date} à {event.time} </p>
+                    <p>Capacité : {event.capacity}</p>
+
+                    <button>Modifier </button>
+                    <button>Supprimer </button>
+
+                </div>
+            ))}
+
+        </div>
+    );
+}
+
+export default AdminDashboard;
