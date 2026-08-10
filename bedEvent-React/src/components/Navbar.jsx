7dcab64427e3997e2ae23909021095bad1c84bd2
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-08-10 13:23:15 +0100

    Un fonction de Navbar qui retour link de 4 fichier

--- bedEvent-React/src/components/Navbar.jsx
+++ bedEvent-React/src/components/Navbar.jsx
@@ -1,1 +1,16 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+
+function Navbar() {
+    return (
+        <nav>
+
+            <NavLink to="/"> Accueil </NavLink>
+            <NavLink to="/login"> Connexion</NavLink> 
+            <NavLink to="/profile/tickets">Mes billets </NavLink>
+            <NavLink to="/admin">Administration</NavLink>
+
+        </nav>
+    );
+}
+
+export default Navbar;
