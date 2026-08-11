43f3ada388b796b5c8bf7a801b072f6f6c47430f
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-08-11 16:05:45 +0100

    Ajouter l'intercepteur de tout les requete et vérifie le token

--- bedEvent-React/src/services/api.js
+++ bedEvent-React/src/services/api.js
@@ -4,3 +4,15 @@ const api=axios.create({
      baseURL : 'http://localhost:8000/api',
 });
 export default api;
+
+
+api.interceptors.request.use(config => {
+
+    const token = localStorage.getItem('token');
+
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+});
