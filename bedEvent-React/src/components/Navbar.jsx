db200273d1ea7f5b144aa0df2a4497968a58440e
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-08-12 14:36:35 +0100

    Ajouter le function logout dans le fichier de navbar

--- bedEvent-React/src/components/Navbar.jsx
+++ bedEvent-React/src/components/Navbar.jsx
@@ -1,8 +1,40 @@
 import { NavLink } from 'react-router-dom';
 
 function Navbar() {
-    return (
+
+
+        const navigate = useNavigate();
+
+        const token = localStorage.getItem('token');
+        const user = JSON.parse(localStorage.getItem('user'));
         
+        function logout() {
+
+        api.post('/logout')
+            .then(response => {
+
+                console.log(response.data);
+
+                localStorage.removeItem('token');
+                localStorage.removeItem('user');
+
+                navigate('/login');
+
+            })
+            .catch(error => {
+
+                console.log(error);
+
+               
+                localStorage.removeItem('token');
+                localStorage.removeItem('user');
+
+                navigate('/login');
+            });
+    }
+
+    return (
+
         <nav>
 
             <NavLink to="/"className={({isActive }) =>isActive? 'font-bold text-blue-600' : 'text-gray-600'}> Accueil </NavLink>
